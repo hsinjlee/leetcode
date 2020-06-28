269af8b954adb4cef1f9c580234610bc92d31a70
hsinjlee <hlee294@asu.edu>
2020-06-28 12:38:39 -0700

    add recursive

--- 145. Binary Tree Postorder Traversal.java
+++ 145. Binary Tree Postorder Traversal.java
@@ -16,3 +16,35 @@ Input: [1,null,2,3]
 Output: [3,2,1]
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
+
+//Recursive
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        return helper(root, new ArrayList());
+    }
+    private List<Integer> helper(TreeNode root, List<Integer> list){
+        if (root == null){
+            return list;
+        }
+        list = helper(root.left, list);
+        list = helper(root.right, list);
+        list.add(root.val);
+        return list;
+    } 
+}
