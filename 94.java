5a0e0126918d871565877fdbb015035f770e57e3
hsinjlee <hlee294@asu.edu>
2020-06-28 11:28:31 -0700

    Create 94.java
    Recursive solution

---
+++ 94.java
@@ -0,0 +1,49 @@
+/*
+94. Binary Tree Inorder Traversal
+Medium
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+//Recursive
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        return helper(root, new ArrayList());
+    }
+    private List<Integer> helper(TreeNode root, List<Integer> list){
+        if (root == null){
+            return list;
+        }
+        list = helper(root.left, list);
+        list.add(root.val);
+        return helper (root.right, list);
+    }
+}
