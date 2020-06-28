f9c16705e4ec21c6938b818e6a9ef974b3c18bc3
hsinjlee <hlee294@asu.edu>
2020-06-28 10:53:52 -0700

    add iterative solution

--- 144.java
+++ 144.java
@@ -35,6 +35,40 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList();
+        Stack <TreeNode> stack = new Stack();
+        while(!stack.isEmpty()  || root != null){
+            while(root != null){
+                stack.push(root);
+                result.add(root.val);
+                root = root.left;
+            }
+            root = stack.pop();
+            root = root.right;
+        }
+        return result;
+    }
+}
+
+//Iteratively
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList();
         Stack <TreeNode> stack = new Stack();
         stack.push(root);
